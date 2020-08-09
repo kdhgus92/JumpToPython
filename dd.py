b044c426d3a1b9e6d4cb6beababa4036baea61c8
Ohhyeun Kwon <kdhgus92@gmail.com>
2020-08-09 23:07:59 +0900

    first commit

---
+++ dd.py
@@ -0,0 +1,14 @@
+print("hello")
+multiline = "Life is too short\nYou need python"
+multiline2 = """
+Life is too short
+You need python
+"""
+
+print(multiline)
+
+head = "Python"
+tail = " is fun!"
+head + tail
+
+print('hello')
---
+++ hello3.py
@@ -0,0 +1,8 @@
+#hello.py
+print("Hello World")
+
+"""
+Author: EungYoungPark
+Date: 2020-07-07
+이 프로그램은 Hello World를 출력하는 프로그램이다.
+"""
