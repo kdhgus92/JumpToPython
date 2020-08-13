7dbf40283db5a069c4fd76cb5b11bbd2b15a7c5f
Ohhyeun Kwon <kdhgus92@gmail.com>
2020-08-13 23:32:21 +0900

    20200813

--- multistring.py
+++ multistring.py
@@ -3,3 +3,18 @@
 print("="*50)
 print("My Program")
 print("="*50)
+
+print("hello")
+multiline = "Life is too short\nYou need python"
+multiline2 = """
+Life is too short
+You need python
+"""
+
+print(multiline)
+
+head = "Python"
+tail = " is fun!"
+head + tail
+
+print('hello')
