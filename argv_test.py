1a96c644f9cb2b1577a3069676f94de14bdfddd2
potash <35871896+potahyox@users.noreply.github.com>
2020-08-17 21:52:14 +0900

    20200817-laptop

---
+++ argv_test.py
@@ -0,0 +1,7 @@
+# #argv_test.py
+# import sys
+# print(sys.argv)
+
+#path_append.py
+import sys
+sys.path.append("C:/doit/mymod")
---
+++ myargv.py
@@ -0,0 +1,6 @@
+import sys
+
+sum = 0
+for i in sys.argv[1:]:
+    sum += int(i)
+print(sum)
