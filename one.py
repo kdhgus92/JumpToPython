738fab3951234780e0ce9be174067a126e235970
Ohhyeun Kwon <kdhgus92@gmail.com>
2020-10-08 23:54:35 +0900

    20201008

--- one.py
+++ one.py
@@ -5,9 +5,15 @@ print('hello')
 def func():
     print("FUNC() IN ONE.PY")
 
+def function1():
+    pass
+
+def function2():
+
 print("TOP LEVEL IN ONE.PY")
 
 if __name__ == "__main__":
+    #run the script
     print('ONE.PY is being run directly!')
 else:
     print('ONE.py has been imported!')
